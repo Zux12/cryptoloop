7995aa4709fccd628007895d7c0c63d586d2d9bf
Zux12 <158550944+Zux12@users.noreply.github.com>
2025-08-02 14:56:08 +0800

    Update BuyRequest.js

--- server/models/BuyRequest.js
+++ server/models/BuyRequest.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 
 const BuyRequestSchema = new mongoose.Schema({
-  user: { type: String, required: true },           // user's email
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  userEmail: {
+    type: String
+  },
   symbol: { type: String, required: true },
   usd: { type: Number, required: true },
   amount: { type: Number, required: true },
-  status: { type: String, default: 'Pending' },     // Pending, Approved, Rejected
+  status: { type: String, default: 'Pending' },
   timestamp: { type: Date, default: Date.now }
 });
 
